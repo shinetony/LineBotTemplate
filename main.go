9d8215e5b30b88da00e998c81d898286c2cfa983
Yu-Chu Lee <Yu-Chu Lee>
2017-01-26 23:30:22 +0800

    add gmail

--- main.go
+++ main.go
@@ -57,7 +57,62 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
+				if err = sendGmail(message.ID+":"+message.Text); err != nil {
+					log.Print(err)
+				}				
 			}
 		}
 	}
+}
+func sendGmail(bodytext string) error {
+	// Reads in our credentials
+	secret, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	// Creates a oauth2.Config using the secret
+	// The second parameter is the scope, in this case we only want to send email
+	conf, err := google.ConfigFromJSON(secret, gmail.GmailSendScope)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+  code := os.Getenv("GmailAuthCode")
+	// Exchange the auth code for an access token
+	tok, err := conf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	// Create the *http.Client using the access token
+	client := conf.Client(oauth2.NoContext, tok)
+
+	// Create a new gmail service using the client
+	gmailService, err := gmail.New(client)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	// New message for our gmail service to send
+	var message gmail.Message
+
+  to_mail := os.Getenv("GmailSentTo")
+	// Compose the message
+	messageStr := []byte(
+		"From: youremail@gmail.com\r\n" +
+			"To: "+ to_mail + "\r\n" +
+			"Subject: LineBot \r\n\r\n" +
+			bodytext)
+
+	// Place messageStr into message.Raw in base64 encoded format
+	message.Raw = base64.URLEncoding.EncodeToString(messageStr)
+
+	// Send the message
+	_, err = gmailService.Users.Messages.Send("me", &message).Do()
+	if err != nil {
+		log.Printf("Error: %v", err)
+	} else {
+		fmt.Println("Message sent!")
+	}
+	return err
 }
